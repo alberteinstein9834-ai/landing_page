5d982ed3ef9fe16a31bc26eaece1fb07a15e8ad4
alberteinstein9834-ai <alberteinstein98.34@gmail.com>
2026-04-14 10:43:02 +0500

    shazzy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Routes/Home';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
